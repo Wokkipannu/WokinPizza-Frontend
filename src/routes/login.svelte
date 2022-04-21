572b8fc5815e2769db7eee137dcec2548d70bf60
Wokki <iiro@kooditaikuri.fi>
2022-04-21 19:53:52 +0300

    New: Added login page

---
+++ src/routes/login.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  function handleClick() {
+    return window.location.href = "http://localhost/api/auth/login"
+  }
+</script>
+
+<script context="module">
+  import axios from 'axios'
+  export async function load() {
+    axios.get('http://localhost/api/auth/user')
+    .then(() => {
+      return window.location.href = "/dashboard"
+    })
+
+    return {}
+  }
+</script>
+
+<svelte:head>
+	<title>Wokin Pizza - Kirjaudu</title>
+	<meta name="description" content="Sukella syvälle pizzojen makumaailmaan" />
+</svelte:head>
+
+<div class="container mx-auto my-5 text-center">
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" on:click={handleClick}>
+    Kirjaudu sisään
+  </button>
+</div>
