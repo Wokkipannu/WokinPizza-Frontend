fc3441f13bfaa27c0acc337293e58ce05f2485e3
Wokki <iiro@kooditaikuri.fi>
2022-04-21 19:53:44 +0300

    New: Pizza modifying functions and authentication

---
+++ src/services/api.ts
@@ -0,0 +1,82 @@
+import axios from 'axios'
+
+export interface Pizza {
+  id: string,
+  created_at: string,
+  updated_at: string,
+  name: string,
+  description: string,
+  toppings: string,
+  image: string,
+  thumbnail: string
+}
+
+export interface User {
+  access_level: string,
+  avatar: string,
+  email: string,
+  exp: number,
+  id: string,
+  token: {
+    access_token: string,
+    refresh_token: string,
+    token_type: string,
+    expiry: string
+  }
+  username: string,
+}
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_PATH,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+export const getPizzas = () => {
+  return API.get('/pizza')
+}
+
+export const getDailyToppings = () => {
+  return API.get('/dailytoppings')
+}
+
+export const loginPath = () => {
+  return import.meta.env.VITE_API_PATH + '/auth/login'
+}
+
+export const logout = () => {
+  return API.get('/auth/logout')
+}
+
+export const getUser = () => {
+  return new Promise((resolve, reject) => {
+    API.get('/auth/user')
+      .then(res => {
+        resolve(res.data.data)
+      })
+      .catch(err => {
+        reject(err)
+      })
+  })
+}
+
+export const updatePizza = (data: Pizza) => {
+  return new Promise((resolve, reject) => {
+    API.put('/pizza', data)
+      .then(res => {
+        resolve(res.data.data)
+      })
+      .catch(err => {
+        reject(err)
+      })
+  })
+}
+
+export const createPizza = (data: Pizza) => {
+  API.post('/pizza', { data })
+}
+
+export const deletePizza = (id: string) => {
+  API.delete('/pizza', { data: { id } })
+}
