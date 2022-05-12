b38214c43c79961a97d1f4ad16b40f2a82d998e3
Wokki <iiro@kooditaikuri.fi>
2022-05-12 21:11:28 +0300

    New: Error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+  /** @ts-ignore */
+  export function load({ error, status }) {
+    return {
+      props: {
+        message: error.message,
+        status: status
+      }
+    };
+  }
+</script>
+
+<script lang="ts">
+  export let message: string
+  export let status: string
+</script>
+
+<div class="container mx-auto text-center my-5 space-y-5">
+  <h1 class="font-bold text-9xl">{status}</h1>
+  <p>{message}</p>
+</div>
